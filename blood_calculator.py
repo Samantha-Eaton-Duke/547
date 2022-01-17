331628020af5ad20f5e37ff7a7bf61a71207b7ec
Samantha Eaton <se165@duke.edu>
2022-01-17 18:08:08 -0500

    Developed HDL_driver to implement HDL workflow

--- blood_calculator.py
+++ blood_calculator.py
@@ -15,4 +15,19 @@ interface()
 def accept_input(test_name)
     entry = input("Enter the {}test result: ".format(test_name))
     return int(entry)
+
+
+
+def check_HDL(HDL_value):
+    if HDL >= 60
+        answer = "Normal"
+    elif 60 > HDL >= 40
+        answer = "Borderline Low"
+    else HDL < 40
+        answer = "Low"
+    return answer
     
+def HDL_driver():
+    HDL_value = accept_input("HDL")
+    classificaton = check_HDL(HDL_value)
+
