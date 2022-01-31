944ae8f2530bb49024be47ef9ec9702fbe604271
Samantha Eaton <se165@duke.edu>
2022-01-31 16:17:00 -0500

    started testing of blood_calculator with check_HDL

---
+++ test_blood_calculator.py
@@ -0,0 +1,20 @@
+# test_blood_calculator.py
+
+
+import pytest
+
+#generic testing functon that takes two inputs, the function input and the expected answer
+
+# @ indicates a decorator
+@pytest.mark.parametrize("input_HDL, expected", [
+    [70, "Normal"],
+    [45, "Borderline Low"],
+    [20, "Low"],
+    ])
+ #will run these values three times in the test funciton    
+    
+def test_check_HDL(input_HDL, expected):
+    from blood_calculator import check_HDL
+    answer = check_HDL(input_HDL)
+    assert answer == expected
+    
