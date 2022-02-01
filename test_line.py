bd4784f662f2180c1f96edc67d7e448017703c80
Samantha Eaton <se165@duke.edu>
2022-02-01 11:46:49 -0500

    updated test function to parametertize the test

--- test_line.py
+++ test_line.py
@@ -1,7 +1,14 @@
+# this module will test the line.py module
 
 
-def test_line_point((x1,y1),(x2,y2), x):
+@pytest.mark.parametrize("x1, y1, x2, y2, x, expected", [
+    [-5, 10, -3, 4, -4, 7],
+    [-5, 10, -3, 4, -3, 4],
+    [-5, 10, -3, 4, -3.5, 5.5],
+    ])
+
+
+def test_line_point(x1,y1,x2,y2, x):
     from line import line_point
-    result = line_point((0,0), (2,2), 1)
-    expected = 1
-    assert result == expected
+    result = line_point(x1,y1,x2,y2, x)
+        assert result == expected
