87e100c108e818c025f0269ea788e034762a9af2
Samantha Eaton <se165@duke.edu>
2022-02-07 13:58:32 -0500

    created files to pass pytest and pycode style

--- line.py
+++ line.py
@@ -1,15 +1,15 @@
 
-def user_input():
+# def user_input():
     # taking two inputs at a time
-    x1 = input("Enter the first point x value: ")
-    y1 = input("Enter the first point y value: ")
-    x2 = input("Enter the second point x value: ")
-    y2 = input("Enter the second point y value: ")
-    x = input("Enter the x intercept: ")
-    out_string = "You have input points ({},{}) and ({},{}), and a x "
-    "intercept of {}".format(x1, y1, x2, y2, x)
-    print(out_string)
-    return int(x1), int(y1), int(x2), int(y2), int(x)
+    #    x1 = input("Enter the first point x value: ")
+    #    y1 = input("Enter the first point y value: ")
+    #    x2 = input("Enter the second point x value: ")
+    #    y2 = input("Enter the second point y value: ")
+    #    x = input("Enter the x intercept: ")
+    #    out_string = "You have input points ({},{}) and ({},{}), and a x "
+    #    "intercept of {}".format(x1, y1, x2, y2, x)
+    #    print(out_string)
+    #    return int(x1), int(y1), int(x2), int(y2), int(x)
 
 
 def calculate_slope(x1, y1, x2, y2, x):
@@ -37,4 +37,5 @@ def line_point(x1, y1, x2, y2, x):
     m = calculate_slope(x1, y1, x2, y2, x)
     b = x_intercept(y1, m, x1)
     y = y_intercept(m, x, b)
+    return y
     print_results(y)
